db1a711c6fa3755b5bc83cb1b81640a8124c4773
UnknonPerson <tanish90065@gmail.com>
2026-04-03 14:54:00 +0530

    Backend folder Structure done

--- backend/src/models/task.modal.js
+++ backend/src/models/task.modal.js
@@ -1,3 +1,26 @@
-import mongoose, { Schema } from 'mongoose'
+import mongoose, { Schema } from "mongoose";
 
-const todo = new Schema();
+const todo_Task = new Schema(
+  {
+    task: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    completed: {
+      type: Boolean,
+      default: false,
+    },
+
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User", 
+    },
+  },
+  {
+    timestamps: true, 
+  }
+);
+
+export const Todo = mongoose.model("Todo", todo_Task);
